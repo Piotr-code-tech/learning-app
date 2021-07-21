dc13255ff822c89459e1a35bece8c2ac94717eee
Piotr-code-tech <piotrkowalski056@gmail.com>
2021-07-21 23:08:42 +0200

    added eventlistener - corrected

--- src/calculator.js
+++ src/calculator.js
@@ -1,11 +1,13 @@
-//Added EventListener for now this returns alert on the page
-document.getElementById("btn-submit").addEventListener("click", alertSubmit);
-document.getElementById("btn-reset").addEventListener("click", alertReset);
+const buttons = [document.querySelector(".btn-submit"), document.querySelector(".btn-reset")];
 
-function alertSubmit() {
-alert ("You clicked submit button");
+function sub_event(){
+alert('Submit button was clicked');
 }
 
-function alertReset() {
-alert ("You clicked reset button");
+function rst_event(){
+alert('Reset button was clicked');
 }
+
+buttons[0].addEventListener('click', sub_event);
+buttons[1].addEventListener('click', rst_event);
+
